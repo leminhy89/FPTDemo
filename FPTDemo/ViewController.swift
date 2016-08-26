771574662247725c79d33da654626723dde80517
yleminh <leminhy89@gmail.com>
2016-08-26 13:28:37 +0700

    Initial Commit

---
+++ FPTDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FPTDemo
+//
+//  Created by Minh Y Le on 8/26/16.
+//  Copyright © 2016 Minh Y Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
